f01db52a2383ffe42c9d1a41588cf66ea8df36b1
Amani215 <amanibrik5@gmail.com>
2022-05-03 11:20:10 +0200

    Fixed: throw exception

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -3,23 +3,23 @@ package hu.bme.mit.spaceship;
 import java.util.Random;
 
 /**
-* Class storing and managing the torpedoes of a ship
-*
-* (Deliberately contains bugs.)
-*/
+ * Class storing and managing the torpedoes of a ship
+ *
+ * (Deliberately contains bugs.)
+ */
 public class TorpedoStore {
 
   // rate of failing to fire torpedos [0.0, 1.0]
-  private double FAILURE_RATE = 0.0; //NOSONAR
+  private double FAILURE_RATE = 0.0; // NOSONAR
 
   private int torpedoCount = 0;
 
-  public TorpedoStore(int numberOfTorpedos){
+  public TorpedoStore(int numberOfTorpedos) {
     this.torpedoCount = numberOfTorpedos;
 
     // update failure rate if it was specified in an environment variable
     String failureEnv = System.getenv("IVT_RATE");
-    if (failureEnv != null){
+    if (failureEnv != null) {
       try {
         FAILURE_RATE = Double.parseDouble(failureEnv);
       } catch (NumberFormatException nfe) {
@@ -28,9 +28,9 @@ public class TorpedoStore {
     }
   }
 
-  public boolean fire(int numberOfTorpedos){
-    if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
-      new IllegalArgumentException("numberOfTorpedos");
+  public boolean fire(int numberOfTorpedos) {
+    if (numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount) {
+      throw new IllegalArgumentException("numberOfTorpedos");
     }
 
     boolean success = false;
@@ -41,7 +41,7 @@ public class TorpedoStore {
 
     if (r >= FAILURE_RATE) {
       // successful firing
-      this.torpedoCount =- numberOfTorpedos;
+      this.torpedoCount = -numberOfTorpedos;
       success = true;
     } else {
       // simulated failure
@@ -51,7 +51,7 @@ public class TorpedoStore {
     return success;
   }
 
-  public boolean isEmpty(){
+  public boolean isEmpty() {
     return this.torpedoCount <= 0;
   }
 
