25fe5ff2d25af53d57c2f8c8540b3eed0a868f4a
cloud <amanibrik5@gmail.com>
2022-05-17 11:41:30 +0200

    Added: JaCoCo

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -45,4 +45,103 @@ public class GT4500Test {
     verify(mockTS2,times(1)).fire(1); 
   }
 
+  // Test #1 
+  //Checks that for the first time the primary store is fired. even if both exist
+  @Test
+  public void fireTorpedo_Single_First_Success(){
+    // Arrange
+    when(mockTS1.fire(1)).thenReturn(true);
+    when(mockTS2.fire(1)).thenReturn(true); 
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    //assertEquals(true, result);
+    verify(mockTS1,times(1)).fire(1); 
+    verify(mockTS2,times(0)).fire(1); 
+  }
+
+  // Test #2
+  // Checks that the firing does alternate
+  @Test
+  public void fireTorpedo_Single_Alternate(){
+    // Arrange
+    when(mockTS1.fire(1)).thenReturn(true);
+    when(mockTS2.fire(1)).thenReturn(true); 
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+    result = ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    //assertEquals(true, result);
+    verify(mockTS1,times(1)).fire(1); 
+    verify(mockTS2,times(1)).fire(1); 
+  }
+  
+  // Test #3
+  // Checks that nothing is fired if both are empty
+  @Test
+  public void fireTorpedo_All_Empty_Failure(){
+    // Arrange
+    when(mockTS1.isEmpty()).thenReturn(true);
+    when(mockTS2.isEmpty()).thenReturn(true); 
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.ALL);
+
+    // Assert
+    //assertEquals(true, result);
+    verify(mockTS1,times(0)).fire(1); 
+    verify(mockTS2,times(0)).fire(1); 
+  }
+
+  // Test #4
+  // Checks that second one is fired if first is empty
+  @Test
+  public void fireTorpedo_Single_First_Empty(){
+    // Arrange
+    when(mockTS1.isEmpty()).thenReturn(true);
+    when(mockTS2.fire(1)).thenReturn(true); 
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    //assertEquals(true, result);
+    verify(mockTS1,times(0)).fire(1); 
+    verify(mockTS2,times(1)).fire(1); 
+  }
+
+
+  // Test #5
+  // Checks that second one is not fired if first fails
+  @Test
+  public void fireTorpedo_Single_First_Fails(){
+    // Arrange
+    when(mockTS1.fire(1)).thenReturn(false);
+    when(mockTS2.fire(1)).thenReturn(true); 
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    //assertEquals(true, result);
+    verify(mockTS1,times(1)).fire(1); 
+    verify(mockTS2,times(0)).fire(1); 
+  }
+
+    // Test #6
+  // Checks that second one is not fired if first fails
+  @Test
+  public void fireTorpedo_Single_Second_First_Empty(){
+    // Arrange
+    when(mockTS1.fire(1)).thenReturn(true);
+    when(mockTS2.isEmpty()).thenReturn(true); 
+
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE); //Fire first
+    result = ship.fireTorpedo(FiringMode.SINGLE); //Fire first
+
+    // Assert
+    //assertEquals(true, result);
+    verify(mockTS1,times(2)).fire(1); 
+    verify(mockTS2,times(0)).fire(1); 
+  }
 }
